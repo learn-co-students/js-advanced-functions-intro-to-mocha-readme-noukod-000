444d032683d7828d2f55cc460f423b1f4d1734dc
Keren-Happuc Fleurimond <keren@noukod.com>
2019-03-12 14:00:22 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return 'Hey!';
+}
+
+function sayHeyFriend(John) {
+  return `Hey, ${John}!`;
+}
+
+function favoriteIceCream(flavor) {
+  return `I love ${flavor}`;
+}
+
+function shouting(string) {
+  return string.toUpperCase();
+}
+
+function roundDown(number) {
+  return Math.floor(number);
+}
